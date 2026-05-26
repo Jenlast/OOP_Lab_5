8c08620529db82104c51b176f4ee70684c9c7be5
Samurai622 <vovik13on@gmail.com>
2026-05-26 17:31:48 +0300

    Refactor project files to update namespace and restore csproj configuration

---
+++ Views/Task1View.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OOP_Lab5.Views;
+
+public partial class Task1View : UserControl
+{
+    public Task1View()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/Task2View.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OOP_Lab5.Views;
+
+public partial class Task2View : UserControl
+{
+    public Task2View()
+    {
+        InitializeComponent();
+    }
+}
