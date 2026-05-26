1992eac6b02d9775221c005c0dadc8520a113691
Janlast <foxyvdov@gmail.com>
2026-05-26 17:10:08 +0300

    Refactor MainWindowViewModel to include current view model management and command methods

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace OOP_Lab5.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace OOP_Lab5.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private ViewModelBase _currentViewModel;
+
+    public MainWindowViewModel()
+    {
+        CurrentViewModel = new Task1ViewModel(); // Відкриваємо Коней за замовчуванням
+    }
+
+    [RelayCommand]
+    private void SwitchToTask1() => CurrentViewModel = new Task1ViewModel();
+
+    [RelayCommand]
+    private void SwitchToTask2() => CurrentViewModel = new Task2ViewModel();
 }
